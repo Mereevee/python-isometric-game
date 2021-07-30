c5bdac7b34e22821c6758605840dfca6e27c0678
Mereevee <71831989+Mereevee@users.noreply.github.com>
2021-07-30 20:19:09 -0300

    :art: Improved isometric equations.

--- isometric.py
+++ isometric.py
@@ -13,16 +13,20 @@ class Isometric:
     def render(self):
         for layer in self.map["layers"]:
             a, b = 0, 0
+
             try:
                 offset = layer["offsety"]
             except:
                 offset = 0
+
             for sprite in layer["data"]:
                 if a == self.rect.width * layer["height"]:
                     a = 0
                     b += self.rect.height
-                x = (((a / self.rect.width) * 0.5 * self.rect.width + (b / self.rect.height) * -0.5 * self.rect.width) - self.rect.width / 2) + self.Settings.width / 2
-                y = ((((a / self.rect.width) * 0.25 * self.rect.width + (b / self.rect.height) * 0.25 * self.rect.width) - (self.rect.height / 2) * 5) - (self.rect.height / 4) + offset) + self.Settings.height / 2
+
+                x = (a - b) / 2 - (self.rect.width / 2) + (self.Settings.width / 2)
+                y = (((a + b) / 2) / 2) - ((self.rect.height / 2) * (layer["height"] / 2)) + (self.Settings.height / 2) - (self.rect.height / 4) + offset
+
                 if sprite != 0:
                     self.screen.blit(self.sprites[sprite], (x, y))
                 a += self.rect.width
